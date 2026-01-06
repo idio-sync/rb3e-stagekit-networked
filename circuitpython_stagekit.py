5c20c47e7aba90a83e8b6569f3ad212354ff34c3
idio-sync <jedivoodoo@gmail.com>
2026-01-06 09:41:50 -0500

    Remove comments about installation and hardware
    Removed installation and hardware instructions from the file.

--- circuitpython_stagekit.py
+++ circuitpython_stagekit.py
@@ -1,24 +1,3 @@
-"""
-Raspberry Pi Pico W 2 - Stage Kit Controller
-CircuitPython version
-
-Receives lighting commands via WiFi (RB3Enhanced UDP packets)
-Controls Santroller Stage Kit via USB
-
-Installation:
-1. Download CircuitPython for Pico W 2 from circuitpython.org
-2. Flash .uf2 file to Pico (hold BOOTSEL, drag file)
-3. Edit this file and save as code.py on CIRCUITPY drive
-4. Update WIFI_SSID and WIFI_PASSWORD below
-5. Connect Stage Kit to Pico via USB OTG
-6. Pico will auto-reboot and start running
-
-Hardware:
-- Raspberry Pi Pico W 2
-- 5V 2A power supply via VBUS (Pin 40) or micro USB
-- Santroller Stage Kit via USB OTG
-"""
-
 import board
 import digitalio
 import time
@@ -31,7 +10,7 @@ import wifi
 import socketpool
 import ipaddress
 
-# USB Host (experimental - check CircuitPython version support)
+# USB Host
 try:
     import usb.core
     import usb.util
